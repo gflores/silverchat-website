af7b74bac5652ca3f822d1f50777d4043f3a68eb
gflores-sleek <gael@sleek-corp.com>
2020-01-19 12:24:34 +0800

    [Automated] Generating Production Files

--- deploy-2-remote.sh
+++ deploy-2-remote.sh
@@ -3,19 +3,6 @@ git pull
 
 projectName="silverchat-website"
 
-cd ~/$projectName/server
-rm -rf dist
-npm install
-npm run build
-
-now=$(pm2 pid $projectName"_server")
-if [ -z $now ]; 
-then
-pm2 start dist/server.js --name $projectName"_server"
-else
-pm2 restart $projectName"_server"
-fi
-
 cd ~/$projectName/client
 npm install --production
 now=$(pm2 pid $projectName"_client")
