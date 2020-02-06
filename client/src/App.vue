013b009ee71bf44854c6aafd54ba2a026fa2373f
gflores-sleek <gael@sleek-corp.com>
2020-02-06 20:15:48 +0800

    [Automated] Generating Production Files

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,7 @@
         padding: 0;
         font-family: 'Source Sans Pro', sans-serif;
         color: #26292c;
+        font-size: 20px;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
     }
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,22 +1,16 @@
 <template lang="pug">
   .home
-    header
-      img.logo(src="images/logo.png")
+    .top
       a.login(:href="appUrl") Login
-    .flex
-      section.s1
-        .title Unlock your website's full potential
-        .subtitle Interact with your visitors in real-time
-        .direct-call
-          .silverchat-location
-        .subtitle Instant voice and video calls to your traffic.
-        //- .fine-text(v-if="instantCallButtonClicked == false") Try instant calling !
-      section.s2
-        .coming-soon Contact: <b>+65 8138 3605</b><br/><br/>Take control of your customer engagement
-      
 
-    //- iframe(width="600px" height="600px" src="https://app.silverchat.co/play/xrp/client" allow="camera;microphone")
+      .logo
+        span.silver Silver
+        span.chat chat
+      .subtitle Unlock your website's full potential
+      .direct-call
+        .silverchat-location
 
+    .bottom
 </template>
 
 <script>
@@ -53,101 +47,202 @@ export default {
     width: 100%;
     position: absolute;
   }
-  header {
+  .top {
+    height: 92%;
+    background:white;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    height: 60px;
+    padding: 40px 20px;
     box-sizing: border-box;
-    padding: 0px 20px;
-    background: hsla(210, 4%, 90%, 1);
-    justify-content: space-between;
 
-    @media only screen and (min-width: 720px) {
-      padding: 0px 50px;
+
+    .login {
+      position: absolute;
+      top: 18.5px;
+      right: 20px;
+      text-decoration: none;
+      color: hsla(210, 4%, 32%, 1);
+      font-size: 18px;
+
+      @media only screen and (min-width: 1080px) {
+        right: 60px;
+        font-size: 20px;
+      }
+
     }
 
     .logo {
-      height: 20px;
+      margin-top: 60px;
+      font-family: 'PantonBlack';
+      font-size: 48px;
+      line-height: 1.2em;
+
+      @media only screen and (min-width: 720px) {
+        margin-top: 30px;
+      }
+      @media only screen and (min-width: 1440px) {
+        margin-top: 45px;
+        font-size: 56px;
+      }
+      @media only screen and (min-width: 1680px) {
+        margin-top: 60px;
+        font-size: 66px;
+      }
+
+      .silver {
+        color: #515f6b;
+      }
+      .chat {
+        color: #26bf26;
+      }
     }
-    .login {
-      text-decoration: none;
-      color: #26292c;
-      font-size: 18px;
+    .subtitle {
+      margin-top: 60px;
+      line-height: 1.25em;
+      font-size: 22px;
+      text-align: center;
+      @media only screen and (min-width: 1440px) {
+        margin-top: 30px;
+        font-size: 26px;
+      }
+      @media only screen and (min-width: 1680px) {
+        margin-top: 40px;
+        font-size: 28px;
+      }
     }
-  }
-  .flex {
-    display: flex;
-    flex-direction: column;
-    height: calc(100% - 60px);
-  }
-  @keyframes fade-in {
-    from {opacity: 0;}
-    to {opacity: 1;}
-  }
-  // @keyframes background-fade-out {
-  //   from {background: hsla(120, 91%, 34%, 1);}
-  //   to {background: initial}
-  // }
-  section {
-    padding: 40px 20px;
 
     .direct-call {
       margin-top: 40px;
       width: 100%;
       max-width: 360px;
-      // transition: height 1s;
-      // animation: fade-in 2s, background-fade-out 5s ease-in;
 
-      animation: fade-in 2s;
-      &.active {
-        height: 355px;
+      @media only screen and (min-width: 1440px) {
+        margin-top: 80px;
+        max-width: 420px;
       }
+      @media only screen and (min-width: 1680px) {
+        margin-top: 120px;
+        max-width: 520px;
+      }
+
       .silverchat-location {
-        height: 55px;
+        height: 60px;
         background: hsla(120, 91%, 34%, 1);
-      }
-    }
 
-    &.s1 {
-      background: white;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
-      .title {
-        font-family: 'PantonBlack';
-        font-size: 34px;
-        text-align: center;
-        line-height: 1.2em;
-        max-width: 500px;
+        @media only screen and (min-width: 1680px) {
+          height: 72px;
+        }
       }
-      .subtitle {
-        margin-top: 20px;
-        font-size: 20px;
-        line-height: 1.5em;
-        text-align: center;
-      }
-
-      .fine-text {
-        margin-top: 20px;
-        font-size: 14px;
-        line-height: 1.5em;
-        text-align: center;
-      }
-    }
-
-    &.s2 {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: #7c8994;
-      flex-grow: 1;
-      color: #e5e6e7;
-      font-size: 20px;
-      // font-weight: bold;
-      padding: 20px;
-      text-align: center;
     }
 
   }
+  .bottom {
+    height: 8%;
+    background: hsla(210, 14%, 6%, 1);
+  }
+
+
+
+
+
+
+
+  // header {
+  //   display: flex;
+  //   align-items: center;
+  //   height: 60px;
+  //   box-sizing: border-box;
+  //   padding: 0px 20px;
+  //   background: hsla(210, 4%, 90%, 1);
+  //   justify-content: space-between;
+
+  //   @media only screen and (min-width: 720px) {
+  //     padding: 0px 50px;
+  //   }
+
+  //   .logo {
+  //     height: 20px;
+  //   }
+  //   .login {
+  //     text-decoration: none;
+  //     color: #26292c;
+  //     font-size: 18px;
+  //   }
+  // }
+  // .flex {
+  //   display: flex;
+  //   flex-direction: column;
+  //   height: calc(100% - 60px);
+  // }
+  // @keyframes fade-in {
+  //   from {opacity: 0;}
+  //   to {opacity: 1;}
+  // }
+  // // @keyframes background-fade-out {
+  // //   from {background: hsla(120, 91%, 34%, 1);}
+  // //   to {background: initial}
+  // // }
+  // section {
+  //   padding: 40px 20px;
+
+  //   .direct-call {
+  //     margin-top: 40px;
+  //     width: 100%;
+  //     max-width: 360px;
+  //     // transition: height 1s;
+  //     // animation: fade-in 2s, background-fade-out 5s ease-in;
+
+  //     animation: fade-in 2s;
+  //     &.active {
+  //       height: 355px;
+  //     }
+  //     .silverchat-location {
+  //       height: 55px;
+  //       background: hsla(120, 91%, 34%, 1);
+  //     }
+  //   }
+
+  //   &.s1 {
+  //     background: white;
+  //     display: flex;
+  //     flex-direction: column;
+  //     align-items: center;
+
+  //     .title {
+  //       font-family: 'PantonBlack';
+  //       font-size: 34px;
+  //       text-align: center;
+  //       line-height: 1.2em;
+  //       max-width: 500px;
+  //     }
+  //     .subtitle {
+  //       margin-top: 20px;
+  //       font-size: 20px;
+  //       line-height: 1.5em;
+  //       text-align: center;
+  //     }
+
+  //     .fine-text {
+  //       margin-top: 20px;
+  //       font-size: 14px;
+  //       line-height: 1.5em;
+  //       text-align: center;
+  //     }
+  //   }
+
+  //   &.s2 {
+  //     display: flex;
+  //     justify-content: center;
+  //     align-items: center;
+  //     background: #7c8994;
+  //     flex-grow: 1;
+  //     color: #e5e6e7;
+  //     font-size: 20px;
+  //     // font-weight: bold;
+  //     padding: 20px;
+  //     text-align: center;
+  //   }
+
+  // }
 </style>
