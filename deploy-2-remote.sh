cfb0fe58c04f9a34abe6aa08cf60cb080363063f
gflores-sleek <gael@sleek-corp.com>
2020-02-07 19:58:15 +0800

    [Automated] Generating Production Files

--- deploy-2-remote.sh
+++ deploy-2-remote.sh
@@ -8,7 +8,7 @@ npm install --production
 now=$(pm2 pid $projectName"_client")
 if [ -z $now ]; 
 then
-pm2 start server.js --name $projectName"_client"
+pm2 start server.js --name $projectName"_client" --time
 else
-pm2 restart $projectName"_client"
+pm2 restart $projectName"_client" --time
 fi
